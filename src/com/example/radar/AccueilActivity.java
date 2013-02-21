d59d1d7270f4230f6382376145a43c352acd41c6
Anna <anna.cejalvo@gmail.com>
2013-02-21 10:16:39 +0100

    radar

---
+++ src/com/example/radar/AccueilActivity.java
@@ -0,0 +1,22 @@
+package com.example.radar;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AccueilActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.accueil);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.accueil, menu);
+		return true;
+	}
+
+}
