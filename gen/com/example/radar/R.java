9dbbf63c55e6a27531269256eb1f60e11dacb262
melaineK <koffiamelaine@gmail.com>
2013-02-21 10:34:51 +0100

    Creation activite PhotoVille

--- gen/com/example/radar/R.java
+++ gen/com/example/radar/R.java
@@ -16,13 +16,16 @@ public final class R {
     public static final class id {
         public static final int btn_alentours=0x7f070001;
         public static final int btn_geo=0x7f070000;
-        public static final int menu_settings=0x7f070002;
+        public static final int button1=0x7f070002;
+        public static final int menu_settings=0x7f070003;
     }
     public static final class layout {
         public static final int accueil=0x7f030000;
+        public static final int activity_photo_ville=0x7f030001;
     }
     public static final class menu {
         public static final int accueil=0x7f060000;
+        public static final int activity_photo_ville=0x7f060001;
     }
     public static final class string {
         public static final int alentours=0x7f040004;
@@ -30,6 +33,7 @@ public final class R {
         public static final int geolocalisation=0x7f040003;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int title_activity_photo_ville=0x7f040005;
     }
     public static final class style {
         /** 
